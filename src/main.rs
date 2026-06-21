08045a85bc0da48da186782bd7e3d4ddd4ab9466
platipus25 <40722163+platipus25@users.noreply.github.com>
2026-06-20 20:24:34 -0700

    Add the rest of the code table
    Hallucinated by Gemini because I didn't feel like typing it out.
    I assume morse code is common enough to be memorized correctly.

--- src/main.rs
+++ src/main.rs
@@ -115,13 +115,112 @@ impl TryFrom<&str> for Sequence {
     }
 }
 
-fn convert_character(character: char) -> Result<&'static [Symbol], MorseAlphabetError> {
-    Ok(match character {
+const fn convert_character(character: char) -> Result<&'static [Symbol], MorseAlphabetError> {
+    let out: &[Symbol] = match character.to_ascii_lowercase() {
+        'a' => &[Symbol::Dit, Symbol::Dah],
+        'b' => &[Symbol::Dah, Symbol::Dit, Symbol::Dit, Symbol::Dit],
+        'c' => &[Symbol::Dah, Symbol::Dit, Symbol::Dah, Symbol::Dit],
+        'd' => &[Symbol::Dah, Symbol::Dit, Symbol::Dit],
         'e' => &[Symbol::Dit],
+        'f' => &[Symbol::Dit, Symbol::Dit, Symbol::Dah, Symbol::Dit],
+        'g' => &[Symbol::Dah, Symbol::Dah, Symbol::Dit],
+        'h' => &[Symbol::Dit, Symbol::Dit, Symbol::Dit, Symbol::Dit],
+        'i' => &[Symbol::Dit, Symbol::Dit],
+        'j' => &[Symbol::Dit, Symbol::Dah, Symbol::Dah, Symbol::Dah],
+        'k' => &[Symbol::Dah, Symbol::Dit, Symbol::Dah],
+        'l' => &[Symbol::Dit, Symbol::Dah, Symbol::Dit, Symbol::Dit],
         'm' => &[Symbol::Dah, Symbol::Dah],
+        'n' => &[Symbol::Dah, Symbol::Dit],
+        'o' => &[Symbol::Dah, Symbol::Dah, Symbol::Dah],
+        'p' => &[Symbol::Dit, Symbol::Dah, Symbol::Dah, Symbol::Dit],
+        'q' => &[Symbol::Dah, Symbol::Dah, Symbol::Dit, Symbol::Dah],
+        'r' => &[Symbol::Dit, Symbol::Dah, Symbol::Dit],
+        's' => &[Symbol::Dit, Symbol::Dit, Symbol::Dit],
+        't' => &[Symbol::Dah],
+        'u' => &[Symbol::Dit, Symbol::Dit, Symbol::Dah],
+        'v' => &[Symbol::Dit, Symbol::Dit, Symbol::Dit, Symbol::Dah],
+        'w' => &[Symbol::Dit, Symbol::Dah, Symbol::Dah],
+        'x' => &[Symbol::Dah, Symbol::Dit, Symbol::Dit, Symbol::Dah],
+        'y' => &[Symbol::Dah, Symbol::Dit, Symbol::Dah, Symbol::Dah],
+        'z' => &[Symbol::Dah, Symbol::Dah, Symbol::Dit, Symbol::Dit],
+
+        // Numbers
+        '1' => &[
+            Symbol::Dit,
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dah,
+        ],
+        '2' => &[
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dah,
+        ],
+        '3' => &[
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dah,
+            Symbol::Dah,
+        ],
+        '4' => &[
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dah,
+        ],
+        '5' => &[
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dit,
+        ],
+        '6' => &[
+            Symbol::Dah,
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dit,
+        ],
+        '7' => &[
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dit,
+            Symbol::Dit,
+            Symbol::Dit,
+        ],
+        '8' => &[
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dit,
+            Symbol::Dit,
+        ],
+        '9' => &[
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dit,
+        ],
+        '0' => &[
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dah,
+            Symbol::Dah,
+        ],
+
         ' ' => &[Symbol::GroupSpace],
-        _ => Err(MorseAlphabetError::UnknownCharacter)?,
-    })
+        _ => return Err(MorseAlphabetError::UnknownCharacter),
+    };
+
+    Ok(out)
 }
 
 #[derive(Debug)]
